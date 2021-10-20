c11829d08d6c6105c7b5cafdf6a230f4847838f8
arimohy <arimohy@gmail.com>
2021-10-20 13:15:45 -0500

    Agregar un icono a la aplicación

--- web/src/components/Navbar.js
+++ web/src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../logoyhomira2.png'
 
 export const PublicNavbar = () => (
   <nav>
     <section>
+      <Link to="/">
+      <img  className="logo" src={Logo} />
+      </Link>
       <Link to="/">Home</Link>
       <Link to="/questions">Questions</Link>
     </section>
@@ -13,6 +17,9 @@ export const PublicNavbar = () => (
 export const PrivateNavbar = () => (
   <nav>
     <section>
+      <Link to="/">
+      <img className="logo"  src={Logo} />
+      </Link>
       <Link to="/">Home</Link>
       <Link to="/questions">Questions</Link>
       <Link to="/new">New</Link>
