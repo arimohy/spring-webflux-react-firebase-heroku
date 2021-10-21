ea5e0890e5f110443c6888c5003ec6fec32495b6
arimohy <arimohy@gmail.com>
2021-10-21 16:55:32 -0500

    Modal y Eliminacion de Pregunta

--- web/src/components/Question.js
+++ web/src/components/Question.js
@@ -1,30 +1,58 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import imagensinusuario from "../userd.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import imagensinusuario from "../userd.png";
+import Swal from 'sweetalert2'
 
 export const Question = ({ question, excerpt, onDelete }) => (
-  <article className={excerpt ? 'question-excerpt' : 'question'}>
-
+  <article className={excerpt ? "question-excerpt" : "question"}>
     <div className="card mb-3">
       <div className="row g-0">
-      <h6 className="card-header">{question.nombre!=null?question.nombre:"usuario sin nombre"}</h6>
-        
-        
+        <h6 className="card-header">
+          {question.nombre != null ? question.nombre : "usuario sin nombre"}
+        </h6>
+
         <div className="col-md-10">
           <div className="card-body">
             <h3 className="card-title">{question.question}</h3>
-            <h5 className="card-text">{question.category}  - <small>{question.type}</small></h5>
-            
+            <h5 className="card-text">
+              {question.category} - <small>{question.type}</small>
+            </h5>
           </div>
         </div>
         <div className="col-md-2">
-        <img src={question.url!=null?question.url:imagensinusuario} alt="img-fluid rounded-end" alt="..." />
+          <img
+            src={question.url != null ? question.url : imagensinusuario}
+            alt="img-fluid rounded-end"
+            alt="..."
+          />
         </div>
       </div>
     </div>
-   
+
     {onDelete && (
-      <button className="button right" onClick={() => onDelete(question.id)}>DELETE</button>
+      <button
+        className="button right"
+        onClick={() => {
+          Swal.fire({
+            title: "¿Estas Seguro que quieres eliminar esta pregunta?",
+            text: "Se eliminaran tambien sus respuestas!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si, Eliminarlo!",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              onDelete(question.id)
+              Swal.fire("Eliminado!", "Su pregunta fue eliminado.", "success");
+            }
+          });
+          //onDelete(question.id)
+        }}
+      >
+        DELETE
+      </button>
+      //<button className="button right">DELETE</button>
     )}
     {excerpt && (
       <Link to={`/question/${question.id}`} className="button">
@@ -32,4 +60,4 @@ export const Question = ({ question, excerpt, onDelete }) => (
       </Link>
     )}
   </article>
-)
+);
